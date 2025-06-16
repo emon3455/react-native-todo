ec4c7b9f144cf55dcd8f5b6d242d4f94702f4bcc
emon3455 <emon.mhk69@gmail.com>
2025-06-16 12:43:03 +0600

    initial commit

---
+++ app/(tabs)/(home)/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Home</Text>
+      <Link
+        href={{
+          pathname: '/details/[id]',
+          params: { id: 'bacon' },
+        }}>
+        View user details
+      </Link>
+      <Link href="/(tabs)/settings">Settings</Link>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function SettingsScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Settings</Text>
+      <Image source={require('../../assets/images/adaptive-icon.png')} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
